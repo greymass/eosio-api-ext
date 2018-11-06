88126a1fb3053f4a6834843ce4f11ec1fac0a2ee
Aaron Cox <aaron@greymass.com>
2018-11-06 16:46:27 -0500

    Implementation of get_currency_balances

---
+++ README.md
@@ -0,0 +1,37 @@
+# Configuration
+
+Each `.env.example` file within the `./configs` folder and subfolders must be copied and potentially modified as an `.env` in the same folder.
+
+```
+cp config/.env.example config/.env
+cp config/get_currency_balances/.env.example config/get_currency_balances/.env
+```
+
+# Running
+
+This entire repository is designed to be run easily under Docker using docker-compose. With both Docker and docker-compose installed, clone this repository, enter the folder and run:
+
+```
+docker-compose build
+docker-compose up -d
+```
+
+The processes specified in the `docker-compose.yaml` file will be automatically configured and started.
+
+# Ports
+
+Each API service will bind to a different IP address on the localhost, which can then be used via a proxy to redirect specific API requests to these new services.
+
+- `/v1/chain/get_currency_balances` runs on port 8901
+
+# nginx configuration
+
+Within an nginx server block, simply add a new entry that redirects to the new local API service.
+
+For example, to run the new `get_currency_balances` API, add a new `location` entry to the `server` block of your host:
+
+```
+location /v1/chain/get_currency_balances$ {
+    proxy_pass http://127.0.0.1:8901;
+}
+```
---
+++ services/get_currency_balances/main.py
@@ -0,0 +1,61 @@
+import json
+import requests
+import os
+import sys
+import falcon
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+tokens = []
+
+def get_tokens():
+    global tokens
+    # Request all tokens from the customtokens smart contract
+    r = requests.post(os.environ['UPSTREAM_API'] + '/v1/chain/get_table_rows', json={
+        "code": "customtokens",
+        "json": True,
+        "limit": 1000,
+        "scope": "customtokens",
+        "table": "tokens"
+    })
+    if r.status_code == 200:
+        temp = []
+        # Iterate over rows and build the tuple
+        for token in r.json().get('rows'):
+            symbol = token.get('customasset').split(" ")[1]
+            temp.append((symbol, token.get('customtoken')))
+        # Set the global cache
+        tokens = temp
+
+class GetCurrencyBalances:
+    def on_post(self, req, resp):
+        # Process the request to retrieve the account name
+        request = json.loads(req.stream.read())
+        # Establish session for retrieval of all balances
+        with requests.Session() as session:
+          balances = []
+          # Iterate and request each tokens balance
+          for (symbol, code) in tokens:
+              # Request balance from upstream API
+              r = requests.post(os.environ['UPSTREAM_API'] + '/v1/chain/get_currency_balance', json={
+                  "account": request.get('account'),
+                  "code": code,
+                  "symbol": symbol
+              })
+              # On success, append to balances
+              if r.status_code == 200:
+                  balances = balances + r.json()
+        # Response with list
+        resp.body = json.dumps(balances)
+
+# Load the initial tokens on startup
+get_tokens()
+
+# Schedule tokens to be refreshed from smart contract every minute
+scheduler = BackgroundScheduler()
+scheduler.add_job(get_tokens, 'interval', minutes=1, id='get_tokens')
+scheduler.start()
+
+# Launch falcon API
+app = falcon.API()
+app.add_route('/v1/chain/get_currency_balances', GetCurrencyBalances())
